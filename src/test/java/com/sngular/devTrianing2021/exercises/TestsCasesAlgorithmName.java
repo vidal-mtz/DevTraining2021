9e1b2d03e06deecc46688ca84ed3101227d62258
Tomás Uriel Hernández Basurto <uriel.hernandez@sngular.com>
2021-04-19 15:16:05 -0500

    add imports to test case

--- src/test/java/com/sngular/devTrianing2021/exercises/TestsCasesAlgorithmName.java
+++ src/test/java/com/sngular/devTrianing2021/exercises/TestsCasesAlgorithmName.java
@@ -1,5 +1,8 @@
 package com.sngular.devTrining2021.test.exercises;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class TestsCasesAlgorithmName {
     @Test
     public void test() {
