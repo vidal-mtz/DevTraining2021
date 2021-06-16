f956ce329d013eb9a07084a9d40733e969e94a0c
Tomas Uriel Hernandez Basurto <uriel.hernandez@medianetsoftware.com>
2021-06-16 14:00:28 -0500

    added some modifications on README and have been added messages

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # DevTraining2021
 
-On this repository, I publish a Exercise of Algorithms on java, for ech one of exercises you need to use the next 
+On this repository, I publish a Exercise of Algorithms on java, for each one of exercises you need to use the next
 command lines:
 
 * Clean project - mvn clean
 * Test the algorithm with Test Unit - mvn test
 
-**Notes:** 
+**Notes:**
+
 * On te creation of projects, we use Apache maven v 3.6.1 
 * This examples use JUnit Lib  v4.13.1
 
--- src/test/java/com/sngular/devTrianing2021/exercises/TestsCasesAlgorithmName.java
+++ src/test/java/com/sngular/devTrianing2021/exercises/TestsCasesAlgorithmName.java
@@ -6,12 +6,12 @@ import static org.junit.Assert.*;
 public class TestsCasesAlgorithmName {
     @Test
     public void test() {
-        assertEquals(3,AlgorithmName.findEvenIndex(new int[] {1,2,3,4,3,2,1}));
-        assertEquals(1,AlgorithmName.findEvenIndex(new int[] {1,100,50,-51,1,1}));
-        assertEquals(-1,AlgorithmName.findEvenIndex(new int[] {1,2,3,4,5,6}));
-        assertEquals(3,AlgorithmName.findEvenIndex(new int[] {20,10,30,10,10,15,35}));
-        assertEquals(-1,AlgorithmName.findEvenIndex(new int[] {-8505, -5130, 1926, -9026}));
-        assertEquals(1,AlgorithmName.findEvenIndex(new int[] {2824, 1774, -1490, -9084, -9696, 23094}));
-        assertEquals(6,AlgorithmName.findEvenIndex(new int[] {4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4}));
+        assertEquals("Test value {1,2,3,4,3,2,1}",3,AlgorithmName.findEvenIndex(new int[] {1,2,3,4,3,2,1}));
+        assertEquals("Test value {1,2,3,4,3,2,1}",1,AlgorithmName.findEvenIndex(new int[] {1,100,50,-51,1,1}));
+        assertEquals("Test value {1,2,3,4,3,2,1}",-1,AlgorithmName.findEvenIndex(new int[] {1,2,3,4,5,6}));
+        assertEquals("Test value {1,2,3,4,3,2,1}",3,AlgorithmName.findEvenIndex(new int[] {20,10,30,10,10,15,35}));
+        assertEquals("Test value {1,2,3,4,3,2,1}",-1,AlgorithmName.findEvenIndex(new int[] {-8505, -5130, 1926, -9026}));
+        assertEquals("Test value {1,2,3,4,3,2,1}",1,AlgorithmName.findEvenIndex(new int[] {2824, 1774, -1490, -9084, -9696, 23094}));
+        assertEquals("Test value {1,2,3,4,3,2,1}",6,AlgorithmName.findEvenIndex(new int[] {4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4}));
     }
 }
