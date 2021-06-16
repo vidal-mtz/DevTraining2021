0191f74a443027e82b76a869f85c7c7acc5563e1
Tomas Uriel Hernandez Basurto <uriel.hernandez@sngular.team>
2021-06-16 12:19:36 -0500

    No Valid
    No Valid

--- src/main/java/com/sngular/devTraining2021/exercises/AlgorithmName.java
+++
@@ -1,8 +0,0 @@
-package com.sngular.devTrining2021.exercises;
-
-public class AlgorithmName {
-    //Implements the function with the algorithm
-    public static int findEvenIndex(int[] arr) {
-        // your code
-    }
-}
