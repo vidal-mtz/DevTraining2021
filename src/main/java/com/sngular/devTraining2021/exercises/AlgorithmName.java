bf523ec5b7aef2ef035a4ce8c0374c9359cb95d6
vidal-mtz <77635311+vidal-mtz@users.noreply.github.com>
2021-06-18 11:39:42 -0500

    Exercise05 - minMax
    Exercise05 - minMax

--- src/main/java/com/sngular/devTraining2021/exercises/AlgorithmName.java
+++ src/main/java/com/sngular/devTraining2021/exercises/AlgorithmName.java
@@ -2,6 +2,16 @@ package com.sngular.devTrining2021.exercises;
 
 public class AlgorithmName {
     public static int[] minMax(int[] arr) {
-        // Your awesome code here
+        		int[] result = new int[] { Integer.MAX_VALUE, Integer.MIN_VALUE };
+
+		for (int value : arr) {
+			if (value < result[0])
+				result[0] = value;
+
+			if (value > result[1])
+				result[1] = value;
+		}
+
+		return result;
     }
 }
