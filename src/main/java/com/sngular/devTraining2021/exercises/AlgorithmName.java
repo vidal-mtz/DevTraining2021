26edcf39fc2fab0cf0c064c1f0db25eb84419fcb
vidal-mtz <77635311+vidal-mtz@users.noreply.github.com>
2021-06-18 11:42:07 -0500

    Exercise04 - findEvenIndex
    Exercise04 - findEvenIndex

--- src/main/java/com/sngular/devTraining2021/exercises/AlgorithmName.java
+++ src/main/java/com/sngular/devTraining2021/exercises/AlgorithmName.java
@@ -2,6 +2,29 @@ package com.sngular.devTrining2021.exercises;
 
 public class AlgorithmName {
     public static int findEvenIndex(int[] arr) {
-        //Implements the function with the algorithm
+        int result = -1;
+		int left = 0;
+		int right = 0;
+		int max = arr.length / 2 + 1;
+		int length = arr.length;
+
+		int i = 1;
+		do {
+			left = sumArray(arr, 0, i);
+			right = sumArray(arr, i + 1, length);
+		} while (left != right && max != i++);
+
+		if (left == right)
+			result = i;
+
+		return result;
+	}
+
+	private static int sumArray(int[] array, int ini, int fin) {
+		int suma = 0;
+		for (int i = ini; i < fin; i += 1) {
+			suma += array[i];
+		}
+		return suma;
     }
 }
